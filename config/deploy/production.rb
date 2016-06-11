7023534429b82586dd9f5e3d01d28d6322065c96
Ezekiel <ezerivadeneiral@gmail.com>
2016-06-11 16:22:59 -0300

    Initial Capistrano config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,6 @@
+require 'dotenv'
+Dotenv.load
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
@@ -7,21 +10,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-
-
-# role-based syntax
-# ==================
-
-# Defines a role with one or multiple servers. The primary server in each
-# group is considered to be the first unless any  hosts have the primary
-# property set. Specify the username and a domain or IP for the server.
-# Don't use `:all`, it's a meta role.
-
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
-
-
+server ENV['AWS_SERVER'], user: ENV['AWS_USER'], roles: %w{web app}
 
 # Configuration
 # =============
@@ -33,19 +22,6 @@
 
 
 
-# Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult the Net::SSH documentation.
-# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
-#
-# Global options
-# --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
